c0dd4a41fe770358eaa44c4b382ab589cd9b6f55
hjkang94 <hjkang94@github.com>
2024-12-21 13:37:45 +0900

    Chore: prettier 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
-import List from "./components/List";
-import Form from "./components/Form";
+import List from './components/List';
+import Form from './components/Form';
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
@@ -14,11 +14,11 @@ export default function App() {
       id: Date.now(),
       title: value,
       completed: false,
-    }
+    };
 
     setTodoData((prev) => [...prev, newTodoData]);
     setValue('');
-  }
+  };
 
   return (
     <div className="container">
@@ -27,11 +27,10 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
 
-        <List todoData={todoData}  setTodoData={setTodoData} />
+        <List todoData={todoData} setTodoData={setTodoData} />
 
         <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
-
       </div>
     </div>
-  )
+  );
 }
