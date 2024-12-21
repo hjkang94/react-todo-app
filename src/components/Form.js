ce8f6414b799f5e9d0da442d15d36864d7b1d10d
hjkang94 <hjkang94@github.com>
2024-12-21 14:06:18 +0900

    Design: tailwind 스타일 적용

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-export default function Form({value, setValue, handleSubmit}) {
+export default function Form({ value, setValue, handleSubmit }) {
   const handleChange = (e) => {
     setValue(e.target.value);
-  }
+  };
 
   return (
-    <form style={{display: 'flex', marginTop: '30px'}} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex pt-2">
       <input
-        type="text" 
-        name="value" 
-        style={{ flex: '10', padding: '5px'}}
+        type="text"
+        name="value"
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
         placeholder="해야 할 일을 입력하세요."
         value={value}
         onChange={handleChange}
       />
       <input
-        type="submit" 
-        value="입력" 
-        className="btn" 
-        style={{ flex: '1' }}
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-300"
+        type="submit"
+        value="입력"
       />
-
     </form>
-  )
+  );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,30 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export default function List({todoData, setTodoData}) {
-  const btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right',
-  }
-
-  const getStyle = (completed) => {
-    return {
-      padding: '20px 10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: completed ? 'line-through' : 'none',
-    }
-  }
-
+export default function List({ todoData, setTodoData }) {
   const handleClick = (id) => {
-    let newTodoData = todoData.filter(data => data.id !== id);
+    let newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
-  }
+  };
 
   const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map(data => {
+    let newTodoData = todoData.map((data) => {
       if (data.id === id) {
         data.completed = !data.completed;
       }
@@ -32,17 +15,28 @@ export default function List({todoData, setTodoData}) {
     });
 
     setTodoData(newTodoData);
-  }
+  };
 
   return (
     <div>
-      {todoData.map(data => (
-          <div style={getStyle(data.completed)} key={data.id}>
-          <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
+      {todoData.map((data) => (
+        <div key={data.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => handleCompleteChange(data.id)}
+              />
+              <span className={data.completed ? 'line-through' : undefined}>{data.title}</span>
+            </div>
+
+            <div className="items-center">
+              <button onClick={() => handleClick(data.id)}>x</button>
+            </div>
+          </div>
         </div>
-        ))}
+      ))}
     </div>
-  )
+  );
 }
